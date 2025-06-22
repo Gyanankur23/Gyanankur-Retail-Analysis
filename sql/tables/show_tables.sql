c5b6ecd46aeaf4e6f6a32d64764362e67611bb7c
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-06-22 23:32:58 +0530

    Create show_tables.sql

---
+++ sql/tables/show_tables.sql
@@ -0,0 +1,8 @@
+-- SQLite
+SELECT name FROM sqlite_master WHERE type='table';
+
+-- MySQL
+SHOW TABLES;
+
+-- PostgreSQL
+ SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';
